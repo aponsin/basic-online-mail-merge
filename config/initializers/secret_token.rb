435edc1265e4fc96f92fff6af10cd807e570aa4f
Alexandre Ponsin <alexandre.ponsin@gmail.com>
2014-01-29 16:24:14 -0500

    init repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BasicOnlineMailMerge::Application.config.secret_token = '0149e6dff2de20558914ccd7e8f78d1c258d14a6fd385ae1fb19c5e5abcd59e4f07ec9fc18b924d5fb60331ff39372ec2bfff8d7bbbd8bf9e7cebc7a31289d4e'
